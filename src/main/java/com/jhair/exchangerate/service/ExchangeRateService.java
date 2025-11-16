2fc80a6753273f1d1d08c74415fae029aa56dcc5
Jhair Lazaro <jahirlazaro@gmail.com>
2025-11-16 17:16:27 -0500

    feat: crear service con metodos básicos

---
+++ src/main/java/com/jhair/exchangerate/service/ExchangeRateService.java
@@ -0,0 +1,75 @@
+package com.jhair.exchangerate.service;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.jhair.exchangerate.mapper.ExchangeRateMapper;
+import com.jhair.exchangerate.model.ExchangeRate;
+import com.jhair.exchangerate.model.dto.CreateExchangeRateRequestDTO;
+import com.jhair.exchangerate.model.dto.ExchangeRateResponseDTO;
+import com.jhair.exchangerate.model.dto.PatchExchangeRateRequestDTO;
+import com.jhair.exchangerate.model.dto.UpdateExchangeRateRequestDTO;
+import com.jhair.exchangerate.repository.ExchangeRateRepository;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class ExchangeRateService {
+    
+    private final ExchangeRateRepository exchangeRateRepository;
+    private final ExchangeRateMapper mapper;
+
+    public Flux<ExchangeRateResponseDTO> findAll(){
+        return exchangeRateRepository.findAll()
+                .map(mapper::toDto);
+    }
+
+    public Mono<ExchangeRateResponseDTO> findById(UUID id){
+        return exchangeRateRepository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("No se encontró el Id : " + id)))
+                .map(mapper::toDto);
+    }
+
+    public Mono<ExchangeRateResponseDTO> findExchangeRate(String originCurrency, String finalCurrency){
+        return exchangeRateRepository.findTopByOriginCurrencyAndFinalCurrencyOrderByDateDesc(originCurrency, finalCurrency)
+                .switchIfEmpty(Mono.error(new RuntimeException("No se encontró el cambio de " + originCurrency + " a " + finalCurrency)))
+                .map(mapper::toDto);
+    }
+
+    public Mono<ExchangeRateResponseDTO> create(CreateExchangeRateRequestDTO dto){
+        ExchangeRate entity = mapper.toEntity(dto);
+        return exchangeRateRepository.save(entity)
+                .map(mapper::toDto);
+    }
+
+    public Mono<ExchangeRateResponseDTO> update(UUID id, UpdateExchangeRateRequestDTO dto){
+        return exchangeRateRepository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("No se encontró el Id : " + id)))
+                .flatMap( foundEntity -> {
+                    mapper.updateFromDto(dto, foundEntity);
+                    return exchangeRateRepository.save(foundEntity);
+                })
+                .map(mapper::toDto);
+    }
+
+    public Mono<ExchangeRateResponseDTO> patch(UUID id, PatchExchangeRateRequestDTO dto){
+        return exchangeRateRepository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("No se encontró el Id : " + id)))
+                .flatMap( foundEntity -> {
+                    mapper.patchFromDto(dto, foundEntity);
+                    return exchangeRateRepository.save(foundEntity);
+                })
+                .map(mapper::toDto);
+    }
+
+    public Mono<Void> delete(UUID id){
+        return exchangeRateRepository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("No se encontró el Id : " + id)))
+                .flatMap(exchangeRateRepository::delete);
+    }
+
+}
