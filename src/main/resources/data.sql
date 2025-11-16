6562b1e31d90a63f3b864a57bacf9b6c08508887
Jhair Lazaro <jahirlazaro@gmail.com>
2025-11-16 03:30:53 -0500

    feat: configura database postgres

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO exchange_rate (origin_currency, final_currency, date_to, value_to) VALUES
+('USD', 'PEN', '2025-11-16', 3.34),
+('PEN', 'USD', '2025-11-16', 0.42),
+('EUR', 'PEN', '2025-11-16', 3.84),
+('PEN', 'EUR', '2025-11-16', 0.48);
